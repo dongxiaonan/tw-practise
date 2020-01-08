a903821698908e57933f301624dd57e3f6876f95
Xiaonan Dong <xiaonandong@XiaonandeMacBook-Pro.local>
2020-01-08 19:16:29 +0800

    #0 create project

---
+++ src/test/java/oneTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class oneTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testOne(){
+        assertTrue(true);
+    }
+}
