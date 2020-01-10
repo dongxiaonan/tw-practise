2b5b565c50cec25b368712643600e17f2c3db0cc
Xiaonan DONG <xndong@thoughtworks.com>
2020-01-10 19:09:08 +0800

    #2.5 Remove useless reference

--- Practise/src/test/java/TW/YuanyinPractise/YuanyinTest.java
+++ Practise/src/test/java/TW/YuanyinPractise/YuanyinTest.java
@@ -1,10 +1,8 @@
 package TW.YuanyinPractise;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 public class YuanyinTest {
     @ParameterizedTest
