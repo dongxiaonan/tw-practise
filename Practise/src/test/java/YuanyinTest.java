135bce237b15b1c3b62b9b0bf55b7a7d44e302ac
Xiaonan DONG <xndong@thoughtworks.com>
2020-01-10 18:41:35 +0800

    #2.1 add first test

---
+++ Practise/src/test/java/YuanyinTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class YuanyinTest {
+    @Test
+    public void ShouldReturnTrueWhenCharIsYuanyin(){
+        Assertions.assertTrue(YuanyinPractise.IsYuanyin('a'));
+    }
+}
