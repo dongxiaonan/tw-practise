e706130a3fe033c4204bdea5388936227aad7d44
Xiaonan DONG <xndong@thoughtworks.com>
2020-01-10 19:08:40 +0800

    #2.4 Add more positive test

--- Practise/src/test/java/TW/YuanyinPractise/YuanyinTest.java
+++ Practise/src/test/java/TW/YuanyinPractise/YuanyinTest.java
@@ -2,10 +2,20 @@ package TW.YuanyinPractise;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class YuanyinTest {
-    @Test
-    public void ShouldReturnTrueWhenCharIsYuanyin(){
-        Assertions.assertTrue(YuanyinPractise.IsYuanyin('a'));
+    @ParameterizedTest
+    @CsvSource({
+            "a, true",
+            "e, true",
+            "i, true",
+            "o, true",
+            "u, true"
+    })
+    public void ShouldReturnIsYuanyin(char letter, boolean expected){
+        Assertions.assertEquals(expected, YuanyinPractise.IsYuanyin(letter));
     }
 }
