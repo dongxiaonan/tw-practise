e92e6e2b9fac30964538244532edea79eb37a017
Xiaonan DONG <xndong@thoughtworks.com>
2020-01-10 19:15:11 +0800

    #2.7 Implement and make test pass.

--- Practise/src/main/java/TW/YuanyinPractise/YuanyinPractise.java
+++ Practise/src/main/java/TW/YuanyinPractise/YuanyinPractise.java
@@ -1,7 +1,20 @@
 package TW.YuanyinPractise;
 
+import java.util.ArrayList;
+
 public class YuanyinPractise {
     public static boolean IsYuanyin(char a) {
-        return true;
+        ArrayList<Character> yuanyinChars = new ArrayList<>();
+        yuanyinChars.add('a');
+        yuanyinChars.add('A');
+        yuanyinChars.add('e');
+        yuanyinChars.add('E');
+        yuanyinChars.add('i');
+        yuanyinChars.add('I');
+        yuanyinChars.add('o');
+        yuanyinChars.add('O');
+        yuanyinChars.add('u');
+        yuanyinChars.add('U');
+        return yuanyinChars.contains(a);
     }
 }
