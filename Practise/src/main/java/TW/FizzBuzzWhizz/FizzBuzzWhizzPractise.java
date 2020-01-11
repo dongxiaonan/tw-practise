33be43eb2a892a0ee42c58f02aa51ebdf764108b
Xiaonan DONG <xndong@thoughtworks.com>
2020-01-11 10:55:52 +0800

    #0 Add example code

---
+++ Practise/src/main/java/TW/FizzBuzzWhizz/FizzBuzzWhizzPractise.java
@@ -0,0 +1,16 @@
+package TW.FizzBuzzWhizz;
+
+public class FizzBuzzWhizzPractise {
+    public static void main(String[] args) {
+        int n = 120;
+
+        for( int i = 0; i < n; i++) {
+            String result = fizzBuzz(i+1);
+            System.out.println(result);
+        }
+    }
+
+    private static String fizzBuzz(int i) {
+        return null;
+    }
+}
