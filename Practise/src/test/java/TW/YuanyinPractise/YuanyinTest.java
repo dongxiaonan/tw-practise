d26b4bfeeeb691e6f0897d7324bf81160ab15519
Xiaonan DONG <xndong@thoughtworks.com>
2020-01-11 10:12:02 +0800

    #6.1 Remove useless method

--- Practise/src/test/java/TW/YuanyinPractise/YuanyinTest.java
+++ Practise/src/test/java/TW/YuanyinPractise/YuanyinTest.java
@@ -8,22 +8,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class YuanyinTest {
     @ParameterizedTest
     @CsvSource({
-            "a, true",
-            "e, true",
-            "i, true",
-            "o, true",
-            "u, true",
-            "A, true",
-            "x, false",
-            "0, false",
-            "&, false"
-    })
-    public void ShouldReturnIsYuanyin(char letter, boolean expected){
-        Assertions.assertEquals(expected, YuanyinPractise.IsYuanyin(letter));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
             "hmm, false", "abc, true", "ab, true"
     })
     public void ShouldReturnCanReplace(String str, boolean expected){
