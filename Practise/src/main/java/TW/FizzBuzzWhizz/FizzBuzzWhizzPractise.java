6d3726e7485a63ae8558ad35acd0709d5e4b055f
Xiaonan DONG <xndong@thoughtworks.com>
2020-01-11 11:05:45 +0800

    #2 Make test pass, get buzz can divided by five

--- Practise/src/main/java/TW/FizzBuzzWhizz/FizzBuzzWhizzPractise.java
+++ Practise/src/main/java/TW/FizzBuzzWhizz/FizzBuzzWhizzPractise.java
@@ -15,6 +15,10 @@ public class FizzBuzzWhizzPractise {
         if (i%3 == 0){
             output += "Fizz";
         }
+
+        if (i%5 == 0){
+            output += "Buzz";
+        }
         return output;
     }
 }
