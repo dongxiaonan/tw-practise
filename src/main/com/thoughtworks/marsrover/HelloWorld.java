562707d5b26bfe1c73438aea891a84eb3308583a
Xiaonan DONG <xndong@thoughtworks.com>
2020-01-13 18:30:57 +0800

    update format

--- src/main/com/thoughtworks/marsrover/HelloWorld.java
+++ src/main/com/thoughtworks/marsrover/HelloWorld.java
@@ -4,9 +4,9 @@ public class HelloWorld {
     private Dependency dependency;
 
     public HelloWorld(Dependency dependency) {
-
         this.dependency = dependency;
     }
+
     public String beenCalled() {
         return dependency.say();
     }
